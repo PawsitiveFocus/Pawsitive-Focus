2406224dbf2e44be1377ae11623bea750ea7a8f9
vaageeshadas <vaageesha.das@gmail.com>
2023-11-11 13:28:07 -0500

    homepage view now contains focus and start button --> focus button takes you to set hour and min

--- lib/Views/HomepageView.dart
+++ lib/Views/HomepageView.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pawsitivefocus/ViewModels/HomepageViewModel.dart';
-
+import 'package:numberpicker/numberpicker.dart';
 
 class HomepageView extends StatefulWidget {
   @override
@@ -9,6 +9,8 @@ class HomepageView extends StatefulWidget {
 
 class HomepageViewState extends State<HomepageView> {
   final HomepageViewModel viewModel = HomepageViewModel();
+  int selectedHours = 0;
+  int selectedMinutes = 0;
 
   //pop up to add new task
   void showAddTaskDialog(BuildContext context) {
@@ -49,37 +51,138 @@ class HomepageViewState extends State<HomepageView> {
     );
   }
 
+
+  void setFocusTime(BuildContext context) {
+
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Select Focus Duration'),
+            content: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                NumberPicker(
+                  value: selectedHours,
+                  minValue: 0,
+                  maxValue: 23,
+                  onChanged: (value) => setState(() => selectedHours = value),
+                ),
+                Text('hours'),
+                NumberPicker(
+                  value: selectedMinutes,
+                  minValue: 0,
+                  maxValue: 59,
+                  onChanged: (value) => setState(() => selectedMinutes = value),
+                ),
+                Text('mins'),
+              ],
+            ),
+            actions: <Widget>[
+              // ... actions ...
+            ],
+          );
+        },
+      );
+
+
+
+
+    /*
+    TimeOfDay selectedTime = TimeOfDay.now(); // Default or previously selected time
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Focus Popup'),
+          content: Text('Select a focus time.'),
+          actions: <Widget>[
+            ElevatedButton(
+              child: Text('Pick Time'),
+              onPressed: () async {
+                final TimeOfDay? picked = await showTimePicker(
+                  context: context,
+                  initialTime: selectedTime,
+                );
+
+                if (picked != null && picked != selectedTime) {
+                  // Do something with the picked time
+                  // For example, update the state or store the time
+                  setState(() {
+                    selectedTime = picked;
+                  });
+                  // You can also close the dialog if needed
+                  // Navigator.of(context).pop();
+                }
+              },
+            ),
+            ElevatedButton(
+              child: Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+            ),
+          ],
+        );
+      },
+    );
+    */
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: ListView.builder(
-        itemCount: viewModel.tasks.length,
-        itemBuilder: (context, index) {
-          return Dismissible(
-            key: Key(viewModel.tasks[index]), // Unique key for each item
-            direction: DismissDirection.horizontal,
-            onDismissed: (direction) {
-              int deletedIndex = index; // Capture the current index in a local variable
-              setState(() {
-                viewModel.deleteTask(deletedIndex); // Call the deleteTask method from the view model
-              });
-            },
-            background: Container(
-              color: Colors.red, // Background color when swiping starts
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.symmetric(horizontal: 20.0),
-              child: Icon(
-                Icons.delete,
-                color: Colors.white,
-              ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: FloatingActionButton(
+              onPressed: () {
+                setFocusTime(context);
+              },
+              child: Icon(Icons.add),
+              tooltip: 'Focus',
             ),
-            child: ListTile(
-              title: Text(viewModel.tasks[index]),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Add your functionality here
+              },
+              child: Text('Start'),
             ),
-            // ... other properties remain the same
-          );
-        },
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: viewModel.tasks.length,
+              itemBuilder: (context, index) {
+                return Dismissible(
+                  key: Key(viewModel.tasks[index]),
+                  direction: DismissDirection.horizontal,
+                  onDismissed: (direction) {
+                    setState(() {
+                      viewModel.deleteTask(index);
+                    });
+                  },
+                  background: Container(
+                    color: Colors.red,
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Icon(Icons.delete, color: Colors.white),
+                  ),
+                  child: ListTile(
+                    title: Text(viewModel.tasks[index]),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -89,4 +192,5 @@ class HomepageViewState extends State<HomepageView> {
       ),
     );
   }
+
 }
