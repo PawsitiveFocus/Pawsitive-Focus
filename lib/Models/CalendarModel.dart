ae82b707c4345e50df0370f68ff50c75db9dd92f
snowzhou0104 <z18235151216@outlook.com>
2023-11-15 22:09:35 -0500

    Update CalendarModel.dart

--- lib/Models/CalendarModel.dart
+++ lib/Models/CalendarModel.dart
@@ -1,13 +1,13 @@
 class CalendarModel {
   List<String> tasks = [
-    'Task 1',
-    'Task 2',
-    'Task 3',
-    'Task 4',
-    'Task 5',
-    'Task 6',
-    'Task 7',
-    'Task 8',
-    'Task 9',
+    'Buy groceries',
+    'Finish report for work',
+    'Call the doctor',
+    'Schedule a haircut',
+    'Read a chapter of a book',
+    'Go for a jog',
+    'Pay bills',
+    'Clean the house',
+    'Attend team meeting',
   ];
 }
